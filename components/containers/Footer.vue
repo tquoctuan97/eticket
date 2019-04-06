dd9488f97d45e12efabe765c63e8521ccc50299d
TuanTQ <tquoctuan97@gmail.com>
2019-04-07 01:39:46 +0700

    update footer

--- components/containers/Footer.vue
+++ components/containers/Footer.vue
@@ -1,6 +1,12 @@
 <template>
   <!--copyright-->
-  <p class="aside__copy-right">© Copyright 2018. All rights reserve</p>
+  <div class="footer">
+    <p class="aside__copy-right">© eTicket App. All rights reserved.</p>
+    <div>
+      <img style="margin-right:20px;" width="140" src="/google_play.png">
+      <img width="140" src="/app_store.png">
+    </div>
+  </div>
   <!--end copyright-->
 </template>
 
@@ -9,11 +15,17 @@ export default {};
 </script>
 
 <style>
-.aside__copy-right {
+.footer {
+  height: 150px;
   width: 100%;
   line-height: 50px;
   font-size: 13px;
   text-align: center;
   color: rgba(#000, 0.6);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 1440px;
+  margin: auto;
 }
 </style>
