a9966a3526bcd0292796d2e546e7a477093285f4
tquoctuanit <tquoctuan97@gmail.com>
2019-04-11 00:38:55 +0700

    Dynamic link drawer

--- components/containers/TopMenu.vue
+++ components/containers/TopMenu.vue
@@ -1,8 +1,32 @@
 <template>
   <header>
     <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list class="pt-2" dense>
-        <v-list-tile v-for="item in links" :key="item.title" @click>
+      <!-- <v-toolbar flat class="transparent">
+        <v-list class="pa-0">
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <img src="https://randomuser.me/api/portraits/men/85.jpg">
+            </v-list-tile-avatar>
+
+            <v-list-tile-content>
+              <v-list-tile-title>John Leider</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>-->
+      <v-toolbar-title>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>account_circle</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Sign in</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-toolbar-title>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+        <v-list-tile v-for="item in links" :key="item.title" :href="item.linkTo">
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
@@ -59,8 +83,8 @@ export default {
   data: () => ({
     drawer: null,
     links: [
-      { title: "Home", icon: "dashboard" },
-      { title: "About", icon: "question_answer" }
+      { title: "Home", icon: "dashboard", linkTo: "/" },
+      { title: "About", icon: "question_answer", linkTo: "/sign-in" }
     ],
     place: "Ho Chi Minh",
     items: [
