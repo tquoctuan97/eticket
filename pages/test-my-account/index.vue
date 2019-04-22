9ee679b169eac3109d717170eb483a4ea549b243
xuanttt <xuanttt@solazu.com>
2019-04-23 02:49:13 +0700

    Add account page

---
+++ pages/test-my-account/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div> 
+        
+    <CustomTabsVue/>
+    </div>
+</template>
+
+<script>
+import CustomTabsVue from '../../components/elements/CustomTabs.vue';
+import InputSearch from "@/components/elements/InputSearch";
+export default {
+    components: {
+        CustomTabsVue,
+        InputSearch
+    }
+}
+</script>
